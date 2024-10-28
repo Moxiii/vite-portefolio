6757c8ebc45a9baebbf507d625ada4489ef471f4
maxime <maxime@MacBook-Pro-de-mac.local>
2024-10-28 09:52:35 +0100

    Try to add G-analystic

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
 import './App.scss'
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, useLocation } from 'react-router-dom'
 import Layout from './components/Layout/Layout.jsx'
 import Home from './Screens/Home/Home.jsx'
 import About from './Screens/About/About.jsx'
@@ -8,7 +8,18 @@ import Contact from './Screens/Contact/Contact.jsx'
 import Cv from './Screens/Cv/Cv.jsx'
 import Projets from './components/Projets/Projets.jsx'
 import "./App.scss"
+import ReactGA from 'react-ga';
+import { useEffect } from 'react'
+const TRACK_ID = "G-QFY45Q5VPP"
+ReactGA.initialize(TRACK_ID);
 function App() {
+  const location = useLocation();
+  useEffect(()=>{
+    ReactGA.pageview(location.pathname + location.search)
+  },[location])
+  useEffect(() => {
+    ReactGA.send({ hitType: "pageview", page: window.location.pathname });
+  }, []);
   return (
     <>
       <Routes>
