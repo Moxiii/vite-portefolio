73811c5d8960998e3a3140649922a99dbf061394
maxime lapouge <dpower632@gmail.com>
2025-05-26 11:36:01 +0200

    Work on responsive Make about more responsive

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -2,18 +2,14 @@ import './Layout.scss'
 import Sidebar from '../Sidebar/Sidebar'
 import { Outlet } from 'react-router-dom'
 import Loader from 'react-loaders'
-import { lazy, Suspense, useEffect } from 'react'
+import { lazy, Suspense } from 'react'
 import { useBreakPoint } from "../../Hook/IsDesktop/useBreakPoint.ts"
 import BurgerMenu from '../../components/Sidebar/Burger/BurgerMenu.tsx'
 const MobileView = lazy(() => import('../../Screens/MobileView/MobileView.jsx'))
 
 export default function Layout() {
 const { isMobile , isTinyDesktop } = useBreakPoint()
-useEffect(() => {
-  if(isTinyDesktop){
-    console.log("tiny");
-  }
-},[isTinyDesktop])
+
 
   return (
     <>
