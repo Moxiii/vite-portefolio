05e618e7adff1ddc9cea33105423b11ee5d1a99a
maxime lapouge <dpower632@gmail.com>
2026-08-16 23:15:37 +0200

    refactor: remove unused components and styles
    - Deleted ZoomText component and its associated styles.
    - Removed SharedLayout component and its styles, including navigation and project display logic.
    - Eliminated Sidebar component and its styles, including navigation links.
    - Removed BurgerMenu component and styles for the burger menu functionality.
    - Deleted QuestionMark component for 3D rendering.
    - Cleaned up constants file and SCSS variables.
    - Removed global CSS styles and main entry point for the application.

--- src/Hook/ErrorBoundary/ErrorBoundaryWrapper.jsx
+++
@@ -1,7 +0,0 @@
-import { useNavigate } from 'react-router-dom';
-import ErrorBoundary from './ErrorBoundary';
-
-export default function ErrorBoundaryWrapper({ children }) {
-  const navigate = useNavigate();
-  return <ErrorBoundary navigate={navigate}>{children}</ErrorBoundary>;
-}
